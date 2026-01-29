edae93db6c2b421b46b8cc28dc1e218b4b2d73c5
Magdalena Domaszczyńska <magdalena.domaszczynska@gmail.com>
2026-01-29 01:11:50 +0100

    Komunikaty reagują dynamicznie, ale są niepoprawne

--- analizator-kredytowy/check_email.php
+++ analizator-kredytowy/check_email.php
@@ -6,9 +6,25 @@ $email = (isset($_POST['email'])) ? $_POST['email'] : '';
 $email = trim($email);
 
 if ($email === '') {
-    echo json_encode(['ok' => false, 'exists' => false]);
+    echo json_encode(['ok' => false, 'message' => 'Podaj adres e-mail!']);
+    //echo json_encode(['ok' => false, 'exists' => false]);
     exit;
 }
-$exists = userEmailExists($email);
 
-echo json_encode(['ok' => true, 'exists' => $exists]);
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    echo json_encode(['ok' => false, 'message' => 'Adres e-mail ma niepoprawny format.']);
+    exit;
+}
+
+$exists = userEmailExists($email);
+if ($exists) {
+    echo json_encode(['ok' => true, 'message' => 'Adres e-mail jest dostępny.']);
+    //echo json_encode(['ok' => false, 'exists' => false]);
+    exit;
+} else {
+    echo json_encode(['ok' => false, 'message' => 'Taki e-mail już istnieje!']);
+    //echo json_encode(['ok' => false, 'exists' => false]);
+    exit;
+}
+
+//echo json_encode(['ok' => true, 'message' => '']);
--- analizator-kredytowy/js/auth_validation.js
+++ analizator-kredytowy/js/auth_validation.js
@@ -6,23 +6,30 @@ let t = null;
 email.addEventListener('input', () => {
     clearTimeout(t);
     t = setTimeout(async () => {
-    const v = email.value.trim(); //zapisuje email do zmiennej v
-    if (!v) { hint.textContent = ''; return; }
+        const v = email.value.trim(); //zapisuje email do zmiennej v
+        if (!v) { hint.textContent = ''; return; }
+        const fd = new FormData();
+        fd.append('email', v);
 
-    if (!email.checkValidity()) {
-    hint.textContent = 'Niepoprawny format e-mail.';
-    hint.classList.remove('hint-success', 'hint-danger');
-    hint.classList.add('hint-danger');
-    return;
-    }
+        if (!email.checkValidity()) {
+            hint.textContent = 'Niepoprawny format e-mail.';
+            hint.classList.remove('hint-success', 'hint-danger');
+            hint.classList.add('hint-danger');
+            return;
+        }
 
-    const res = await fetch('check_email.php?email=' + encodeURIComponent(v));
-    const data = await res.json();
-    const exists = data.exists;
+        //const res = await fetch('check_email.php?email=' + encodeURIComponent(v));
+        const res = await fetch('check_email.php', { method: 'POST', body: fd });
+        const data = await res.json();
+        //const exists = data.exists; //data.ok
 
-    hint.textContent = exists ? 'Taki e-mail już istnieje' : 'E-mail dostępny';
-    hint.classList.remove('hint-success', 'hint-danger');
-    hint.classList.add(exists ? 'hint-danger' : 'hint-success');
+        hint.textContent = data.ok ? '' : data.message;
+        hint.classList.remove('hint-success', 'hint-danger');
+        hint.classList.add(data.ok ? 'hint-danger' : 'hint-success');
+
+    //hint.textContent = exists ? 'Taki e-mail już istnieje' : 'E-mail dostępny';
+   // hint.classList.remove('hint-success', 'hint-danger');
+    //hint.classList.add(exists ? 'hint-danger' : 'hint-success');
     }, 350); // debounce ~350ms
 });
 
