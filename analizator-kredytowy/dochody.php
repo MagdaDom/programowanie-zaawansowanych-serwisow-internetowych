613746c2de4c6051b4ba1cab119dfdf8b6fe9d0b
Magdalena Domaszczyńska <magdalena.domaszczynska@gmail.com>
2026-01-30 22:32:41 +0100

    Skończona walidacja głównego formularza - początek dodawania dochodów

--- analizator-kredytowy/dochody.php
+++ analizator-kredytowy/dochody.php
@@ -6,19 +6,6 @@ session_start();
 if (empty($_SESSION['logged'])) {
     header('Location: login.php'); exit;
 }
-//$oprocentowanie = readCsvToTable("src/oprocentowanie.csv");
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['oblicz'])) {
-    if ($_POST['oblicz']) {
-        $session_id = session_id();
-
-        // 3. Zapisz
-        //$stmt = $pdo->prepare("INSERT INTO wyniki (wynik_id, session_id, user_email, zdolnosc, created_at) VALUES (?, ?, ?, ?, NOW())");
-        //$stmt->execute([$wynik_id, $session_id, $_SESSION['user_email'], $zdolnosc]);
-        //nowe id dla nowego wyniku
-        session_regenerate_id(false);
-    }
-}
 ?>
 <!DOCTYPE html>
 <html lang="pl">
@@ -40,8 +27,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['oblicz'])) {
     <?php endif; ?>
 
     <form method="GET">
-        <h3>Informacje o kredytobiorcy</h3>
+        <h3>Podaj swoje źródła dochodów</h3>
         <hr class="section-divider">
+        <!--<select name="rodzaj">
+            <?php foreach ($oprocentowanie as $rodzaj => $rrso): ?>
+                <option value="<?php echo $rodzaj; ?>">
+                    <?php echo $rodzaj; ?> (<?php echo $rrso; ?>%)
+                </option>
+            <?php endforeach; ?>
+        </select>-->
         <label>Łączne dochody:</label>
         <div class="inline-input">
             <input class="add-input" type="number" id="income-display" value="" required
@@ -57,34 +51,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['oblicz'])) {
             <button class="add-button" type="wydatki">Dodaj <i class="bi bi-plus"></i><i class="bi bi-credit-card"></i></button>
         </div>
         <input type="text" name="debt-source" placeholder="Użyj przycisku Dodaj+ by dodać wydatki" disabled readonly>
-
-        <label>Wiek kredytobiorcy:</label>
-        <input type="number" name="age" id="age" min="18" max="65" step = 1 required>
-        <p class="hint">W przypadku kilku współkredytobiorców, podaj datę urodzenia najstarszego/najstarszej z nich.</p>
-        <label>Liczba osób w gospodarstwie domowym:</label>
-        <input type="number" name="people" id="people" min = 1 max = 20 step = 1 required>
-        <p class="hint">Osoby zamieszkujące razem i wspólnie się utrzymujące.</p>
-
-        </br>
-        <h3>Parametry kredytu</h3>
-        <hr class="section-divider">
-        <label>Okres kredytowania:</label>
-        <input type="number" name="years" id="years" value="15" min="1" max="40" step="1" required>
-        <label>Rodzaj raty:</label>
-        <select name="installment" id="installment" required>
-            <option value="" disabled selected hidden>wybierz...</option>
-            <option value="fixed">stała</option>
-            <option value="declining">malejąca</option>
-        </select>
-        <label>Rodzaj oprocentowania:</label>
-        <select name="rate" id="rate" required>
-            <option value="" disabled selected hidden>wybierz...</option>
-            <option value="5">zmienne</option>
-            <option value="0">stałe</option>
-            <option value="2.5">okresowo stałe</option>
-        </select>
-        <label>Oprocentowanie kredytu (RRSO):</label>
-        <input type="number" name="rrso" id="rrso" value = 7.13 min = 1 max = 25 step = 0.01 required>
         <button type="submit">OBLICZ ZDOLNOŚĆ KREDYTOWĄ</button>
     </form>
 
--- analizator-kredytowy/src/auth.php
+++ analizator-kredytowy/src/auth.php
@@ -1,21 +1,5 @@
 <?php
-function openDbConnection() {
-    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT); // wyjątki [web:431]
-    try {
-        $db = new mysqli("127.0.0.1", "root", "", "kalkulatorkredytowy");
-        $db->set_charset("utf8mb4"); // polskie znaki + pełne UTF-8 [web:346]
-        return $db;
-    } catch (mysqli_sql_exception $e) {
-        // zapisywanie błędów do logów
-        error_log("DB connection error: " . $e->getMessage()); // [web:493]
-        // komunikat “dla usera”
-        exit("Błąd połączenia z bazą danych. Spróbuj ponownie później.");
-    }
-}
-
-function closeDbConnection($db) {
-    mysqli_close($db);
-}
+require_once __DIR__ . '/src/functions.php';
 
 function userEmailExists($email) {
     $db=openDbConnection();
