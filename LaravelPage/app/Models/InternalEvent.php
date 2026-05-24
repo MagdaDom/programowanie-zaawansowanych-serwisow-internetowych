2d657b23c9dc92c85342a2e4d7395e12db2a77db
Magdalena Domaszczyńska <magdalena.domaszczynska@gmail.com>
2026-05-24 22:16:47 +0200

    Start of project modification

--- LaravelPage/app/Models/InternalEvent.php
+++ LaravelPage/app/Models/InternalEvent.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class InternalEvent extends Model
 {
-    //
+    protected $table = 'internalevents';
+    protected $primaryKey = 'Id';
+    public $timestamps = false;
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class, 'InternalEventId', 'Id');
+    }
 }
--- LaravelPage/routes/web.php
+++ LaravelPage/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get("/", [HomeController::class, "index"]);
+Route::resource('tasks', TaskController::class);
