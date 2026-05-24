0a702bcbe8a5e2441063e279de378eae0acaf7fa
Magdalena Domaszczyńska <magdalena.domaszczynska@gmail.com>
2026-05-24 22:57:30 +0200

    Dodano stronę główną?

--- LaravelPage/resources/views/home/index.blade.php
+++ LaravelPage/resources/views/home/index.blade.php
@@ -1,7 +1,19 @@
-@extends('main')
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LaravelPage</title>
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+</head>
+<body>
+    <div class="container py-5">
+        <h1 class="mb-4">System CRUD</h1>
+        <p class="lead">Wybierz moduł, który chcesz otworzyć.</p>
 
-@section('content')
-
-Hello world!
-
-@endsection
+        <div class="d-flex gap-3 mt-4">
+            <a href="{{ route('tasks.index') }}" class="btn btn-primary">Przejdź do Tasks</a>
+        </div>
+    </div>
+</body>
+</html>
